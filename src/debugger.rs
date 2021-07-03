2918f2c7dc83b7aeedb01b917f6df7dd743eb58e
Marc Dominik Migge <marcmigge@gmx.net>
2021-07-03 22:01:01 +0200

    Fix breakpoint handling and add basic command parser for debugger

--- src/debugger.rs
+++ src/debugger.rs
@@ -1,23 +1,111 @@
-use nom::IResult;
+use std::io::Write;
+use nom::{
+    IResult,
+    branch::alt,
+    bytes::complete::{tag, take_while, take_while1, take_while_m_n},
+    combinator::map_res,
+    sequence::{delimited, separated_pair}
+};
 
 use crate::bus;
 use crate::cpu;
 use crate::memory;
 
 
+#[derive(Debug)]
 enum Command {
     ToggleBreakpoint(u16),
     ShowCpuRegisters,
     Step(u16),
-    Run,
+    Continue,
     Disassemble(u16),
 }
 
 impl Command {
     fn parse(input: &str) -> IResult<&str, Command> {
-        Ok((input, Command::Run))
+        alt((
+            parse_toggle_breakpoint,
+            parse_show_cpu_registers,
+            parse_step,
+            parse_continue,
+            parse_disassemble,
+        ))(input)
     }
 }
+
+fn is_decimal_digit(c: char) -> bool {
+    c.is_digit(10)
+}
+
+fn from_decimal_str(input: &str) -> Result<u16, std::num::ParseIntError> {
+    u16::from_str_radix(input, 10)
+}
+
+fn parse_decimal(input: &str) -> IResult<&str, u16> {
+    map_res(
+        take_while_m_n(1, 5, is_decimal_digit),
+        from_decimal_str
+    )(input)
+}
+
+fn is_hex_digit(c: char) -> bool {
+    c.is_digit(16)
+}
+
+fn from_hex_str(input: &str) -> Result<u16, std::num::ParseIntError> {
+    u16::from_str_radix(input, 16)
+}
+
+fn parse_hex(input: &str) -> IResult<&str, u16> {
+    let (input, _) = tag("0x")(input)?;
+    map_res(
+        take_while_m_n(1, 4, is_hex_digit),
+        from_hex_str
+    )(input)
+}
+
+fn is_whitespace(c: char) -> bool {
+    c.is_whitespace()
+}
+
+fn parse_toggle_breakpoint(input: &str) -> IResult<&str, Command> {
+    let (input, (_, address)) = delimited(
+        take_while(is_whitespace),
+        separated_pair(
+            alt((tag("breakpoint"), tag("break"), tag("b"))),
+            take_while1(is_whitespace),
+            alt((parse_hex, parse_decimal))
+        ),
+        take_while(is_whitespace)
+    )(input)?;
+
+    Ok((input, Command::ToggleBreakpoint(address)))
+}
+
+fn parse_show_cpu_registers(input: &str) -> IResult<&str, Command> {
+    let (input, _) = alt((tag("registers"), tag("reg"), tag("r")))(input)?;
+
+    Ok((input, Command::ShowCpuRegisters))
+}
+
+fn parse_step(input: &str) -> IResult<&str, Command> {
+    let (input, _) = alt((tag("step"), tag("s")))(input)?;
+
+    Ok((input, Command::Step(0)))
+}
+
+fn parse_continue(input: &str) -> IResult<&str, Command> {
+    let (input, _) = alt((tag("continue"), tag("cont"), tag("c")))(input)?;
+
+    Ok((input, Command::Continue))
+}
+
+fn parse_disassemble(input: &str) -> IResult<&str, Command> {
+    let (input, _) = alt((tag("disassemble"), tag("dis"), tag("d")))(input)?;
+
+    Ok((input, Command::Disassemble(10)))
+}
+
 
 pub struct Debugger<M, B> {
     cpu: cpu::CPUShared<M, B>,
@@ -43,6 +131,11 @@ where
     }
 
     pub fn is_active(&mut self) -> bool {
+        let address = self.cpu.borrow().registers.read_word(&cpu::Register16::PC);
+        if self.breakpoint_at(address) {
+            return true;
+        }
+
         match self.countdown {
             Some(countdown) => {
                 if countdown == 0 {
@@ -62,8 +155,11 @@ where
         let (instruction, _) = self.cpu.borrow_mut().decoder.decode_at(address);
         println!("{:#06x}: {}", address, &instruction);
 
-        let mut input = String::new();
         loop {
+            print!("> ");
+            std::io::stdout().flush().unwrap(); // TODO: is the a better way to handle the result?
+
+            let mut input = String::new();
             match std::io::stdin().read_line(&mut input) {
                 Ok(_) => {
                     if let Ok((_, command)) = Command::parse(&input) {
@@ -74,15 +170,20 @@ where
                                 } else {
                                     self.add_breakpoint(address);
                                 }
+                                
+                                println!("Active breakpoints:");
+                                for breakpoint in &self.breakpoints {
+                                    println!("{:#06x}", breakpoint);
+                                }
                             }
                             Command::ShowCpuRegisters => {
                                 self.cpu.borrow().print_state();
                             }
-                            Command::Step(count) => {
-                                self.countdown = Some(count);
+                            Command::Step(skip) => {
+                                self.countdown = Some(skip);
                                 break;
                             }
-                            Command::Run => {
+                            Command::Continue => {
                                 break;
                             }
                             Command::Disassemble(count) => {
@@ -95,7 +196,6 @@ where
                             }
                         }
                     }
-                    unimplemented!();
                 }
                 Err(error) => {
                     println!("Error reading from stdin: {}", error);
