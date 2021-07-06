070e87a9af7607430bfcb530a6919440983b2b92
Marc Dominik Migge <marcmigge@gmx.net>
2021-07-06 18:33:46 +0200

    Add dummy FDC implementation

--- src/fdc.rs
+++ src/fdc.rs
@@ -13,10 +13,30 @@ impl FloppyDiskController {
     }
 
     pub fn read_byte(&self, port: u16) -> u8 {
-        unimplemented!()
+        match port {
+            0xfb7e => 0, // TODO: read main status register
+            _ => {
+                println!("FDC read {:#06x}", port);
+                unimplemented!()
+            }
+        }
     }
 
     pub fn write_byte(&mut self, port: u16, value: u8) {
-        unimplemented!()
+        match port {
+            0xfa7e => {
+                match value {
+                    0 => (), // TODO: switch motor off
+                    _ => {
+                        println!("FDC write {:#06x} {:#010b}", port, value);
+                        unimplemented!()
+                    }
+                }
+            }
+            _ => {
+                println!("FDC write {:#06x} {:#010b}", port, value);
+                unimplemented!()
+            }
+        }
     }
 }
