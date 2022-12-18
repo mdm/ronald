16cb71de8ba0d02203f307f7fc4486386c8046d0
Marc Dominik Migge <marcmigge@gmx.net>
2022-12-18 16:19:58 +0100

    Add DSK loading via drag and drop

--- ronald-web/src/App.tsx
+++ ronald-web/src/App.tsx
@@ -1,5 +1,5 @@
 import type { Component } from 'solid-js';
-import { onMount } from 'solid-js';
+import { createSignal, onMount } from 'solid-js';
 import init, { Emulator } from '../ronald-wasm/pkg/ronald_wasm';
 
 import logo from './logo.svg';
@@ -13,36 +13,72 @@ const keymap = await response.json();
 await init();
 const TARGET_FRAME_MS = 20;
 
-const step = (emulator: Emulator) => {
+const step = (emulator: Emulator | null) => {
+  if (!emulator) {
+    return;
+  }
   const start = performance.now();
   emulator.step_driver();
   const elapsed = performance.now() - start;
   setTimeout(step.bind(null, emulator), Math.max(0, TARGET_FRAME_MS - elapsed));
 };
 
+const handleOnDrop = async (emulator: Emulator | null, event: DragEvent) => {
+  console.log(event);
+  event.preventDefault();
+
+  let file;
+  if (event.dataTransfer?.items) {
+      if (event.dataTransfer.items[0].kind === 'file') {
+        file = event.dataTransfer.items[0].getAsFile();
+      }
+  } else {
+    file = event.dataTransfer?.files[0];
+  }
+
+  if (file && emulator) {
+    console.log(file.name, file.size); 
+    const buffer = await file.arrayBuffer();
+    emulator.load_disk(0, buffer);
+  }
+}
+
+const handleOnDragOver = (event: DragEvent) => {
+  event.preventDefault();
+}
+
 const App: Component = () => {
+  const [emulator, setEmulator] = createSignal<Emulator | null>(null);
+
   let canvas: HTMLCanvasElement | undefined;
   onMount(() => {
-    const emulator = new Emulator(canvas);
-    document.addEventListener('keydown', ((emulator: Emulator, event: KeyboardEvent) => {
-      if (keymap[event.key]) {
+    setEmulator(new Emulator(canvas));
+
+    document.addEventListener('keydown', ((emulator: Emulator | null, event: KeyboardEvent) => {
+      if (emulator && keymap[event.key]) {
         for (const key of keymap[event.key]) {
           emulator.press_key(key);    
         }
       }
-    }).bind(null, emulator));
-    document.addEventListener('keyup', ((emulator: Emulator, event: KeyboardEvent) => {
-      if (keymap[event.key]) {
+    }).bind(null, emulator()));
+    document.addEventListener('keyup', ((emulator: Emulator | null, event: KeyboardEvent) => {
+      if (emulator && keymap[event.key]) {
         for (const key of keymap[event.key]) {
           emulator.release_key(key);
         }
       }
-    }).bind(null, emulator));
-    step(emulator);
+    }).bind(null, emulator()));
+    step(emulator());
   });
 
   return (
-    <canvas ref={canvas} width="768" height="560" />
+    <canvas
+      ref={canvas}
+      onDrop={[handleOnDrop, emulator()]}
+      onDragOver={handleOnDragOver}
+      width="768"
+      height="560"
+    />
   );
 };
 
