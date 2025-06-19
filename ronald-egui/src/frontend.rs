0aa4a0fb5116aa97f0a2437bd317ce0f55834993
Marc Dominik Migge <marcmigge@gmx.net>
2025-06-19 16:46:02 +0200

    Hook up emulator to egui

--- ronald-egui/src/frontend.rs
+++ ronald-egui/src/frontend.rs
@@ -1,2 +1,102 @@
+use std::{
+    thread::sleep,
+    time::{Duration, Instant},
+};
+
+use eframe::{egui, egui_wgpu};
+
+use ronald_core::{
+    Driver,
+    constants::{SCREEN_BUFFER_HEIGHT, SCREEN_BUFFER_WIDTH},
+    system::System,
+};
+
+use crate::frontend::{audio::CpalAudio, video::EguiWgpuVideo};
+
 mod audio;
-pub mod video;
+mod video;
+
+pub struct Frontend<S>
+where
+    S: System<'static> + 'static,
+{
+    driver: Driver<S>,
+    audio: CpalAudio,
+    video: EguiWgpuVideo,
+    frame_start: Instant,
+    time_available: usize,
+}
+
+impl<S> Frontend<S>
+where
+    S: System<'static> + 'static,
+{
+    pub fn new(render_state: &egui_wgpu::RenderState) -> Self {
+        let driver = Driver::new();
+        let audio = CpalAudio::new();
+        let video = EguiWgpuVideo::new(render_state);
+
+        Self {
+            driver,
+            audio,
+            video,
+            frame_start: Instant::now(),
+            time_available: 0,
+        }
+    }
+
+    pub fn ui(&mut self, ctx: &egui::Context, ui: Option<&mut egui::Ui>) {
+        match ui {
+            Some(ui) => {
+                let size = ui.min_rect().size();
+
+                self.step_emulation();
+                self.draw_framebuffer(ctx, ui, size);
+            }
+            None => {
+                let size = egui::Vec2::new(SCREEN_BUFFER_WIDTH as f32, SCREEN_BUFFER_HEIGHT as f32);
+
+                egui::Window::new("Screen")
+                    .collapsible(false)
+                    .resizable(false)
+                    .default_size(size)
+                    .show(ctx, |ui| {
+                        self.step_emulation();
+                        self.draw_framebuffer(ctx, ui, size);
+                    });
+            }
+        }
+    }
+
+    fn draw_framebuffer(&mut self, _ctx: &egui::Context, ui: &mut egui::Ui, size: egui::Vec2) {
+        let texture = egui::load::SizedTexture::new(self.video.framebuffer(), size);
+        ui.image(texture);
+    }
+
+    fn step_emulation(&mut self) {
+        log::trace!("Starting new frame");
+        let start = Instant::now();
+
+        self.time_available += self.frame_start.elapsed().as_micros() as usize;
+        self.frame_start = Instant::now();
+
+        // TODO: Allow running at 60Hz
+        while self.time_available >= 20_000 {
+            log::trace!("Stepping emulator for 20_000 microseconds");
+            self.driver.step(20_000, &mut self.video, &mut self.audio);
+            self.time_available -= 20_000; // TODO:: take into account actually executed cycles
+        }
+
+        if self.time_available > 0 {
+            log::trace!("Stepping emulator for {} microseconds", self.time_available);
+            self.driver
+                .step(self.time_available, &mut self.video, &mut self.audio);
+            self.time_available = 0; // TODO:: take into account actually executed cycles
+        }
+
+        log::trace!(
+            "Frame emulated in {} microseconds",
+            start.elapsed().as_micros()
+        );
+    }
+}
--- ronald-egui/src/frontend/video.rs
+++ ronald-egui/src/frontend/video.rs
@@ -1,3 +1,5 @@
+use eframe::egui;
+use eframe::egui_wgpu;
 use eframe::wgpu;
 
 use ronald_core::VideoSink;
@@ -7,37 +9,69 @@ pub struct EguiWgpuVideo {
     queue: wgpu::Queue,
     texture: wgpu::Texture,
     texture_view: wgpu::TextureView,
+    framebuffer_texture_id: egui::TextureId,
 }
 
 impl EguiWgpuVideo {
-    pub fn new(device: &wgpu::Device, queue: wgpu::Queue) -> Self {
+    pub fn new(render_state: &egui_wgpu::RenderState) -> Self {
         let texture_extent = wgpu::Extent3d {
             width: SCREEN_BUFFER_WIDTH as u32,
             height: SCREEN_BUFFER_HEIGHT as u32,
             depth_or_array_layers: 1,
         };
 
-        let texture = device.create_texture(&wgpu::TextureDescriptor {
-            label: Some("Framebuffer Texture"),
-            size: texture_extent,
-            mip_level_count: 1,
-            sample_count: 1,
-            dimension: wgpu::TextureDimension::D2,
-            format: wgpu::TextureFormat::Rgba8UnormSrgb,
-            usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
-            view_formats: &[],
-        });
+        let texture = render_state
+            .device
+            .create_texture(&wgpu::TextureDescriptor {
+                label: Some("Framebuffer Texture"),
+                size: texture_extent,
+                mip_level_count: 1,
+                sample_count: 1,
+                dimension: wgpu::TextureDimension::D2,
+                format: wgpu::TextureFormat::Rgba8UnormSrgb,
+                usage: wgpu::TextureUsages::TEXTURE_BINDING | wgpu::TextureUsages::COPY_DST,
+                view_formats: &[],
+            });
+
+        let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
+
+        let framebuffer_texture_id = render_state.renderer.write().register_native_texture(
+            &render_state.device,
+            &texture_view,
+            wgpu::FilterMode::Linear,
+        );
+
+        Self {
+            queue: render_state.queue.clone(),
+            texture,
+            texture_view,
+            framebuffer_texture_id,
+        }
+    }
+
+    pub fn framebuffer(&self) -> egui::TextureId {
+        self.framebuffer_texture_id
+    }
+}
+
+impl VideoSink for EguiWgpuVideo {
+    fn draw_frame(&mut self, buffer: &Vec<u8>) {
+        let texture_extent = wgpu::Extent3d {
+            width: SCREEN_BUFFER_WIDTH as u32,
+            height: SCREEN_BUFFER_HEIGHT as u32,
+            depth_or_array_layers: 1,
+        };
 
         let bytes_per_pixel = 4;
         let bytes_per_row = SCREEN_BUFFER_WIDTH as u32 * bytes_per_pixel;
-        queue.write_texture(
+        self.queue.write_texture(
             wgpu::TexelCopyTextureInfo {
-                texture: &texture,
+                texture: &self.texture,
                 mip_level: 0,
                 origin: wgpu::Origin3d::ZERO,
                 aspect: wgpu::TextureAspect::All,
             },
-            &vec![255; bytes_per_pixel as usize * SCREEN_BUFFER_WIDTH * SCREEN_BUFFER_HEIGHT],
+            &buffer,
             wgpu::TexelCopyBufferLayout {
                 offset: 0,
                 bytes_per_row: Some(bytes_per_row),
@@ -45,29 +79,5 @@ impl EguiWgpuVideo {
             },
             texture_extent,
         );
-
-        let texture_view = texture.create_view(&wgpu::TextureViewDescriptor::default());
-
-        Self {
-            queue,
-            texture,
-            texture_view,
-        }
-    }
-
-    pub fn framebuffer(&self) -> &wgpu::TextureView {
-        &self.texture_view
-    }
-}
-
-impl VideoSink for EguiWgpuVideo {
-    fn draw_frame(&mut self, buffer: &Vec<(u8, u8, u8)>) {
-        let mut frame = vec![0u8; 4 * SCREEN_BUFFER_WIDTH * SCREEN_BUFFER_HEIGHT];
-        for (i, pixel) in frame.chunks_exact_mut(4).enumerate() {
-            pixel[0] = buffer[i].0; // R
-            pixel[1] = buffer[i].1; // G
-            pixel[2] = buffer[i].2; // B
-            pixel[3] = 255; // A
-        }
     }
 }
