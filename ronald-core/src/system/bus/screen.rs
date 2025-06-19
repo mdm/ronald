683d7e1d0dd8c0d47d2ff96d1d6fbabe4e4e020b
Marc Dominik Migge <marcmigge@gmx.net>
2025-06-19 14:57:48 +0200

    Change internal framebuffer representation to RGBA

--- ronald-core/src/system/bus/screen.rs
+++ ronald-core/src/system/bus/screen.rs
@@ -8,34 +8,34 @@ const VIRTUAL_BUFFER_HEIGHT: usize = 39 * 16;
 const BORDER_WIDTH: usize = 4 * 16;
 
 #[allow(clippy::identity_op, clippy::eq_op)]
-const FIRMWARE_COLORS: [(u8, u8, u8); 27] = [
-    (0x00, 0x00, 0x00), // 0
-    (0x00, 0x00, 0x80), // 1
-    (0x00, 0x00, 0xff), // 2
-    (0x80, 0x00, 0x00), // 3
-    (0x80, 0x00, 0x80), // 4
-    (0x80, 0x00, 0xff), // 5
-    (0xff, 0x00, 0x00), // 6
-    (0xff, 0x00, 0x80), // 7
-    (0xff, 0x00, 0xff), // 8
-    (0x00, 0x80, 0x00), // 9
-    (0x00, 0x80, 0x80), // 10
-    (0x00, 0x80, 0xff), // 11
-    (0x80, 0x80, 0x00), // 12
-    (0x80, 0x80, 0x80), // 13
-    (0x80, 0x80, 0xff), // 14
-    (0xff, 0x80, 0x00), // 15
-    (0xff, 0x80, 0x80), // 16
-    (0xff, 0x80, 0xff), // 17
-    (0x00, 0xff, 0x00), // 18
-    (0x00, 0xff, 0x80), // 19
-    (0x00, 0xff, 0xff), // 20
-    (0x80, 0xff, 0x00), // 21
-    (0x80, 0xff, 0x80), // 22
-    (0x80, 0xff, 0xff), // 23
-    (0xff, 0xff, 0x00), // 24
-    (0xff, 0xff, 0x80), // 25
-    (0xff, 0xff, 0xff), // 26
+const FIRMWARE_COLORS: [[u8; 4]; 27] = [
+    [0x00, 0x00, 0x00, 0xff], // 0
+    [0x00, 0x00, 0x80, 0xff], // 1
+    [0x00, 0x00, 0xff, 0xff], // 2
+    [0x80, 0x00, 0x00, 0xff], // 3
+    [0x80, 0x00, 0x80, 0xff], // 4
+    [0x80, 0x00, 0xff, 0xff], // 5
+    [0xff, 0x00, 0x00, 0xff], // 6
+    [0xff, 0x00, 0x80, 0xff], // 7
+    [0xff, 0x00, 0xff, 0xff], // 8
+    [0x00, 0x80, 0x00, 0xff], // 9
+    [0x00, 0x80, 0x80, 0xff], // 10
+    [0x00, 0x80, 0xff, 0xff], // 11
+    [0x80, 0x80, 0x00, 0xff], // 12
+    [0x80, 0x80, 0x80, 0xff], // 13
+    [0x80, 0x80, 0xff, 0xff], // 14
+    [0xff, 0x80, 0x00, 0xff], // 15
+    [0xff, 0x80, 0x80, 0xff], // 16
+    [0xff, 0x80, 0xff, 0xff], // 17
+    [0x00, 0xff, 0x00, 0xff], // 18
+    [0x00, 0xff, 0x80, 0xff], // 19
+    [0x00, 0xff, 0xff, 0xff], // 20
+    [0x80, 0xff, 0x00, 0xff], // 21
+    [0x80, 0xff, 0x80, 0xff], // 22
+    [0x80, 0xff, 0xff, 0xff], // 23
+    [0xff, 0xff, 0x00, 0xff], // 24
+    [0xff, 0xff, 0x80, 0xff], // 25
+    [0xff, 0xff, 0xff, 0xff], // 26
 ];
 
 const HARDWARE_TO_FIRMWARE_COLORS: [usize; 32] = [
@@ -76,7 +76,7 @@ const HARDWARE_TO_FIRMWARE_COLORS: [usize; 32] = [
 #[derive(Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
 pub struct Screen {
-    buffer: Vec<(u8, u8, u8)>,
+    buffer: Vec<u8>,
     gun_position: usize,
     width_counter: usize,
     waiting_for_vsync: bool,
@@ -85,7 +85,7 @@ pub struct Screen {
 impl Screen {
     pub fn new() -> Self {
         Screen {
-            buffer: vec![FIRMWARE_COLORS[0]; SCREEN_BUFFER_WIDTH * SCREEN_BUFFER_HEIGHT],
+            buffer: vec![0; SCREEN_BUFFER_WIDTH * SCREEN_BUFFER_HEIGHT * 4],
             gun_position: 0,
             width_counter: 0,
             waiting_for_vsync: true,
@@ -97,16 +97,17 @@ impl Screen {
             return;
         }
 
-        let buffer_len = self.buffer.len();
+        let color = FIRMWARE_COLORS[HARDWARE_TO_FIRMWARE_COLORS[color]];
+        let buffer_len = self.buffer.len(); // TODO: document why we need this
         let x = (self.gun_position + BORDER_WIDTH) % VIRTUAL_BUFFER_WIDTH;
         let y = (self.gun_position + BORDER_WIDTH) / VIRTUAL_BUFFER_WIDTH;
 
         if y >= 4 * 16 && x < 48 * 16 {
             let y = y - (4 * 16);
-            self.buffer[(y * SCREEN_BUFFER_WIDTH + x) % buffer_len] =
-                FIRMWARE_COLORS[HARDWARE_TO_FIRMWARE_COLORS[color]];
-            self.buffer[(y * SCREEN_BUFFER_WIDTH + x + SCREEN_BUFFER_WIDTH) % buffer_len] =
-                FIRMWARE_COLORS[HARDWARE_TO_FIRMWARE_COLORS[color]];
+            let start = (y * SCREEN_BUFFER_WIDTH + x) * 4 % buffer_len;
+            self.buffer[start..(start + 4)].copy_from_slice(&color);
+            let start = (y * SCREEN_BUFFER_WIDTH + x + SCREEN_BUFFER_WIDTH) * 4 % buffer_len;
+            self.buffer[start..(start + 4)].copy_from_slice(&color);
         }
 
         self.gun_position += 1;
