e60b7772996e522dd23f057b992fd659e4906567
Marc Dominik Migge <marcmigge@gmx.net>
2025-06-19 16:46:02 +0200

    Hook up emulator to egui

--- ronald-egui/src/lib.rs
+++ ronald-egui/src/lib.rs
@@ -1,40 +1,18 @@
-use std::time::Instant;
-
-use eframe::{egui, wgpu};
+use eframe::egui;
 use serde::{Deserialize, Serialize};
 
-use ronald_core::constants::{SCREEN_BUFFER_HEIGHT, SCREEN_BUFFER_WIDTH};
+use ronald_core::system::CPC464;
 
-use frontend::video::EguiWgpuVideo;
+use frontend::Frontend;
 
 mod frontend;
 
-#[derive(Deserialize, Serialize)]
+#[derive(Default, Deserialize, Serialize)]
 #[serde(default)]
 pub struct RonaldApp {
-    label: String,
     screen_only: bool,
     #[serde(skip)]
-    hovering_screen: Option<Instant>,
-    #[serde(skip)]
-    last_hover_pos: Option<egui::Pos2>,
-    #[serde(skip)]
-    frontend_video: Option<EguiWgpuVideo>,
-    #[serde(skip)]
-    framebuffer_texture_id: Option<egui::TextureId>,
-}
-
-impl Default for RonaldApp {
-    fn default() -> Self {
-        Self {
-            label: "Hello, Ronald!".to_string(),
-            screen_only: false,
-            hovering_screen: None,
-            last_hover_pos: None,
-            frontend_video: None,
-            framebuffer_texture_id: None,
-        }
-    }
+    frontend: Option<Frontend<CPC464>>,
 }
 
 impl RonaldApp {
@@ -48,93 +26,47 @@ impl RonaldApp {
 }
 impl eframe::App for RonaldApp {
     fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-        egui::CentralPanel::default().show(ctx, |ui| {
-            ui.label(&self.label);
+        // self.screen_only = true;
+
+        egui::TopBottomPanel::top("menu_bar").show(ctx, |ui| {
+            egui::menu::bar(ui, |ui| {
+                ui.menu_button("View", |ui| {
+                    if ui
+                        .add(egui::Button::new("Emulator Only").selected(self.screen_only))
+                        .clicked()
+                    {
+                        self.screen_only = true;
+                    }
+                    if ui
+                        .add(egui::Button::new("Workbench").selected(!self.screen_only))
+                        .clicked()
+                    {
+                        self.screen_only = false;
+                    }
+                });
+            });
         });
 
-        if let (Some(render_state), None) = (&frame.wgpu_render_state, &self.frontend_video) {
-            // TODO: pass full render state to EguiWgpuVideo and have framebuffer() return a TextureId
-            let frontend_video =
-                EguiWgpuVideo::new(&render_state.device, render_state.queue.clone());
+        egui::CentralPanel::default().show(ctx, |ui| {
+            if let Some(frontend) = &mut self.frontend {
+                if self.screen_only {
+                    frontend.ui(ctx, Some(ui));
+                }
+            }
+        });
 
-            let framebuffer_texture_id = render_state.renderer.write().register_native_texture(
-                &render_state.device,
-                frontend_video.framebuffer(),
-                wgpu::FilterMode::Linear,
-            );
+        if let (Some(render_state), None) = (&frame.wgpu_render_state, &self.frontend) {
+            let frontend = Frontend::new(render_state);
 
-            self.frontend_video = Some(frontend_video);
-            self.framebuffer_texture_id = Some(framebuffer_texture_id);
+            self.frontend = Some(frontend);
         }
 
-        let framebuffer_size =
-            egui::Vec2::new(SCREEN_BUFFER_WIDTH as f32, SCREEN_BUFFER_HEIGHT as f32);
-
-        let mut screen = egui::Window::new("Screen")
-            .collapsible(false)
-            .resizable(false)
-            .default_size(framebuffer_size)
-            .show(ctx, |ui| {
-                if let Some(texture_id) = self.framebuffer_texture_id {
-                    let texture = egui::load::SizedTexture::new(texture_id, framebuffer_size);
-                    ui.image(texture);
-                }
-                let window_rect = ui.min_rect();
-                let pointer_pos = ctx.pointer_hover_pos();
-
-                if let Some(pos) = pointer_pos {
-                    if window_rect.contains(pos) {
-                        if self.last_hover_pos != pointer_pos {
-                            self.hovering_screen = Some(Instant::now());
-                        }
-                    } else {
-                        self.hovering_screen = None;
-                    }
-                    self.last_hover_pos = pointer_pos;
-                }
-
-                if let Some(hovering) = self.hovering_screen {
-                    if hovering.elapsed().as_secs() < 5 {
-                        egui::show_tooltip_at(
-                            ctx,
-                            ui.layer_id(),
-                            egui::Id::new("screen_tooltip"),
-                            window_rect.min,
-                            |ui| {
-                                if ui
-                                    .button(if self.screen_only {
-                                        "Workbench"
-                                    } else {
-                                        "Screen Only"
-                                    })
-                                    .clicked()
-                                {
-                                    self.screen_only = !self.screen_only;
-                                }
-                            },
-                        );
-                    } else {
-                        self.hovering_screen = None;
-                    }
-                }
-            });
+        if let Some(frontend) = &mut self.frontend {
+            if !self.screen_only {
+                frontend.ui(ctx, None);
+            }
+        }
 
         ctx.request_repaint();
     }
 }
-
-// // Setup winit + wgpu yourself
-// let event_loop = EventLoop::new();
-// let window = WindowBuilder::new().build(&event_loop).unwrap();
-//
-// // Initialize wgpu
-// let instance = wgpu::Instance::default();
-// let surface = unsafe { instance.create_surface(&window) }.unwrap();
-// let adapter = instance.request_adapter(...).await.unwrap();
-// let (device, queue) = adapter.request_device(...).await.unwrap();
-//
-// // Setup egui context and egui_wgpu renderer
-// let egui_ctx = egui::Context::default();
-// let mut egui_wgpu_renderer = egui_wgpu::Renderer::new(&device, ...);
-//
-// // Now you have access to `device` and `queue` freely
