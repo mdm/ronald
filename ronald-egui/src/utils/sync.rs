e338b4847de33e67683a77ca1b3679d6942cd6d2
Marc Dominik Migge <marcmigge@gmx.net>
2025-08-23 14:11:48 +0200

    Add a wrapper for shared types that works on WASM and native

---
+++ ronald-egui/src/utils/sync.rs
@@ -0,0 +1,54 @@
+pub trait SharedExt<T> {
+    fn with_mut<R>(&self, f: impl FnOnce(&mut T) -> R) -> R;
+    fn try_with_mut<R>(&self, f: impl FnOnce(&mut T) -> R) -> Option<R>;
+}
+
+#[cfg(target_arch = "wasm32")]
+mod wasm {
+    use std::{rc::Rc, cell::RefCell};
+    use super::SharedExt;
+    
+    pub type Shared<T> = Rc<RefCell<T>>;
+    
+    pub fn shared<T>(value: T) -> Shared<T> {
+        Rc::new(RefCell::new(value))
+    }
+    
+    impl<T> SharedExt<T> for Shared<T> {
+        fn with_mut<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
+            f(&mut *self.borrow_mut())
+        }
+        
+        fn try_with_mut<R>(&self, f: impl FnOnce(&mut T) -> R) -> Option<R> {
+            self.try_borrow_mut().ok().map(|mut guard| f(&mut *guard))
+        }
+    }
+}
+
+#[cfg(not(target_arch = "wasm32"))]
+mod native {
+    use std::sync::{Arc, Mutex};
+    use super::SharedExt;
+    
+    pub type Shared<T> = Arc<Mutex<T>>;
+    
+    pub fn shared<T>(value: T) -> Shared<T> {
+        Arc::new(Mutex::new(value))
+    }
+    
+    impl<T> SharedExt<T> for Shared<T> {
+        fn with_mut<R>(&self, f: impl FnOnce(&mut T) -> R) -> R {
+            f(&mut *self.lock().unwrap())
+        }
+        
+        fn try_with_mut<R>(&self, f: impl FnOnce(&mut T) -> R) -> Option<R> {
+            self.try_lock().ok().map(|mut guard| f(&mut *guard))
+        }
+    }
+}
+
+#[cfg(target_arch = "wasm32")]
+pub use wasm::*;
+
+#[cfg(not(target_arch = "wasm32"))]
+pub use native::*;
