9a24557ba77ff701bffbc5c44165920976262e52
Marc Dominik Migge <marcmigge@gmx.net>
2025-09-05 08:12:56 +0200

    Test subscribe/unsubscribe for debug events

--- ronald-core/src/debug.rs
+++ ronald-core/src/debug.rs
@@ -9,31 +9,58 @@ pub trait DebugEventSubscriber {
     fn on_event(&self, source: DebugSource, event: &DebugEvent);
 }
 
+#[derive(Debug, Clone, Copy, PartialEq, Eq)]
+pub struct SubscriptionHandle {
+    id: usize,
+    source: DebugSource,
+}
+
+struct SubscriberEntry {
+    id: usize,
+    subscriber: Box<dyn DebugEventSubscriber>,
+}
+
 struct SubscriberRegistry {
-    subscribers: HashMap<DebugSource, Vec<Box<dyn DebugEventSubscriber>>>,
+    subscribers: HashMap<DebugSource, Vec<SubscriberEntry>>,
+    next_id: usize,
 }
 
 impl SubscriberRegistry {
     fn new() -> Self {
         Self {
             subscribers: HashMap::new(),
+            next_id: 0,
         }
     }
 
-    fn subscribe(&mut self, source: DebugSource, sub: Box<dyn DebugEventSubscriber>) {
-        self.subscribers.entry(source).or_default().push(sub);
+    fn subscribe(
+        &mut self,
+        source: DebugSource,
+        sub: Box<dyn DebugEventSubscriber>,
+    ) -> SubscriptionHandle {
+        let id = self.next_id;
+        self.next_id += 1;
+
+        let handle = SubscriptionHandle { id, source };
+        let entry = SubscriberEntry {
+            id,
+            subscriber: sub,
+        };
+
+        self.subscribers.entry(source).or_default().push(entry);
+        handle
     }
 
-    fn unsubscribe(&mut self, source: DebugSource, sub: Box<dyn DebugEventSubscriber>) {
-        if let Some(source_subs) = self.subscribers.get_mut(&source) {
-            source_subs.retain(|s| !std::ptr::eq(&**s, &*sub));
+    fn unsubscribe(&mut self, handle: SubscriptionHandle) {
+        if let Some(source_subs) = self.subscribers.get_mut(&handle.source) {
+            source_subs.retain(|entry| entry.id != handle.id);
         }
     }
 
     fn emit(&self, source: DebugSource, event: DebugEvent) {
         if let Some(list) = self.subscribers.get(&source) {
-            for sub in list {
-                sub.on_event(source, &event);
+            for entry in list {
+                entry.subscriber.on_event(source, &event);
             }
         }
     }
@@ -47,12 +74,12 @@ fn emit_event(source: DebugSource, event: DebugEvent) {
     REGISTRY.with(|reg| reg.borrow().emit(source, event));
 }
 
-pub fn subscribe(source: DebugSource, sub: Box<dyn DebugEventSubscriber>) {
-    REGISTRY.with(|reg| reg.borrow_mut().subscribe(source, sub));
+pub fn subscribe(source: DebugSource, sub: Box<dyn DebugEventSubscriber>) -> SubscriptionHandle {
+    REGISTRY.with(|reg| reg.borrow_mut().subscribe(source, sub))
 }
 
-pub fn unsubscribe(source: DebugSource, sub: Box<dyn DebugEventSubscriber>) {
-    REGISTRY.with(|reg| reg.borrow_mut().subscribe(source, sub));
+pub fn unsubscribe(handle: SubscriptionHandle) {
+    REGISTRY.with(|reg| reg.borrow_mut().unsubscribe(handle));
 }
 
 pub trait Snapshotable {
@@ -81,4 +108,183 @@ pub enum DebugSource {
     Ppi,
     Psg,
     Tape,
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use std::cell::RefCell;
+    use std::rc::Rc;
+
+    struct TestSubscriber {
+        events: Rc<RefCell<Vec<(DebugSource, DebugEvent)>>>,
+    }
+
+    impl TestSubscriber {
+        fn new() -> (Self, Rc<RefCell<Vec<(DebugSource, DebugEvent)>>>) {
+            let events = Rc::new(RefCell::new(Vec::new()));
+            let subscriber = Self {
+                events: events.clone(),
+            };
+            (subscriber, events)
+        }
+    }
+
+    impl DebugEventSubscriber for TestSubscriber {
+        fn on_event(&self, source: DebugSource, event: &DebugEvent) {
+            self.events.borrow_mut().push((source, event.clone()));
+        }
+    }
+
+    #[test]
+    fn test_subscribe_single_subscriber() {
+        let (subscriber, events) = TestSubscriber::new();
+
+        subscribe(DebugSource::Cpu, Box::new(subscriber));
+
+        emit_event(
+            DebugSource::Cpu,
+            DebugEvent::Cpu(event::CpuDebugEvent::Register8Changed {
+                register: crate::system::cpu::Register8::A,
+                is: 0x42,
+                was: 0x00,
+            }),
+        );
+
+        let events = events.borrow();
+        assert_eq!(events.len(), 1);
+        assert!(matches!(&events[0], (DebugSource::Cpu, DebugEvent::Cpu(_))));
+    }
+
+    #[test]
+    fn test_subscribe_multiple_subscribers_same_source() {
+        let (subscriber1, events1) = TestSubscriber::new();
+        let (subscriber2, events2) = TestSubscriber::new();
+
+        subscribe(DebugSource::Memory, Box::new(subscriber1));
+        subscribe(DebugSource::Memory, Box::new(subscriber2));
+
+        emit_event(
+            DebugSource::Memory,
+            DebugEvent::Memory(event::MemoryDebugEvent::Test),
+        );
+
+        assert_eq!(events1.borrow().len(), 1);
+        assert_eq!(events2.borrow().len(), 1);
+    }
+
+    #[test]
+    fn test_subscribe_multiple_sources() {
+        let (subscriber, events) = TestSubscriber::new();
+        let (subscriber2, events2) = TestSubscriber::new();
+
+        subscribe(DebugSource::Cpu, Box::new(subscriber));
+        subscribe(DebugSource::Memory, Box::new(subscriber2));
+
+        emit_event(
+            DebugSource::Cpu,
+            DebugEvent::Cpu(event::CpuDebugEvent::Register8Changed {
+                register: crate::system::cpu::Register8::A,
+                is: 0x42,
+                was: 0x00,
+            }),
+        );
+        emit_event(
+            DebugSource::Memory,
+            DebugEvent::Memory(event::MemoryDebugEvent::Test),
+        );
+
+        let events = events.borrow();
+        assert_eq!(events.len(), 1);
+        assert!(matches!(&events[0], (DebugSource::Cpu, DebugEvent::Cpu(_))));
+
+        let events2 = events2.borrow();
+        assert_eq!(events2.len(), 1);
+        assert!(matches!(
+            &events2[0],
+            (DebugSource::Memory, DebugEvent::Memory(_))
+        ));
+    }
+
+    #[test]
+    fn test_unsubscribe_removes_subscriber() {
+        let (subscriber, events) = TestSubscriber::new();
+
+        let handle = subscribe(DebugSource::Cpu, Box::new(subscriber));
+
+        emit_event(
+            DebugSource::Cpu,
+            DebugEvent::Cpu(event::CpuDebugEvent::Register8Changed {
+                register: crate::system::cpu::Register8::A,
+                is: 0x42,
+                was: 0x00,
+            }),
+        );
+        assert_eq!(events.borrow().len(), 1);
+
+        unsubscribe(handle);
+
+        emit_event(
+            DebugSource::Cpu,
+            DebugEvent::Cpu(event::CpuDebugEvent::Register8Changed {
+                register: crate::system::cpu::Register8::A,
+                is: 0x43,
+                was: 0x42,
+            }),
+        );
+        assert_eq!(events.borrow().len(), 1); // Should still be 1, no new events
+    }
+
+    #[test]
+    fn test_unsubscribe_only_removes_matching_subscriber() {
+        let (subscriber1, events1) = TestSubscriber::new();
+        let (subscriber2, events2) = TestSubscriber::new();
+
+        let handle1 = subscribe(DebugSource::Cpu, Box::new(subscriber1));
+        let handle2 = subscribe(DebugSource::Cpu, Box::new(subscriber2));
+
+        unsubscribe(handle1);
+
+        emit_event(
+            DebugSource::Cpu,
+            DebugEvent::Cpu(event::CpuDebugEvent::Register8Changed {
+                register: crate::system::cpu::Register8::A,
+                is: 0x42,
+                was: 0x00,
+            }),
+        );
+
+        assert_eq!(events1.borrow().len(), 0); // First subscriber removed
+        assert_eq!(events2.borrow().len(), 1); // Second subscriber still active
+    }
+
+    #[test]
+    fn test_unsubscribe_with_invalid_handle() {
+        let invalid_handle = SubscriptionHandle {
+            id: 999,
+            source: DebugSource::Cpu,
+        };
+        unsubscribe(invalid_handle); // Should not panic
+    }
+
+    #[test]
+    fn test_emit_with_no_subscribers() {
+        emit_event(
+            DebugSource::Cpu,
+            DebugEvent::Cpu(event::CpuDebugEvent::Register8Changed {
+                register: crate::system::cpu::Register8::A,
+                is: 0x42,
+                was: 0x00,
+            }),
+        ); // Should not panic
+    }
+
+    #[test]
+    fn test_double_unsubscribe() {
+        let (subscriber, _events) = TestSubscriber::new();
+        let handle = subscribe(DebugSource::Cpu, Box::new(subscriber));
+
+        unsubscribe(handle);
+        unsubscribe(handle); // Should not panic
+    }
 }
